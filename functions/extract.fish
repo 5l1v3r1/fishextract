e23deb0fa47a7498df0f17fa0117fc27e2d3b89c
misterCh0c <misterch0c@users.noreply.github.com>
2016-03-18 09:38:16 +0100

    I should stop coding after 10pm

--- functions/extract.fish
+++ functions/extract.fish
@@ -1,27 +1,27 @@
 function extract
     if test -f $argv
         switch $argv
-            case *.rar
+            case \*.rar
                 unrar x $argv
-            case *.zip
+            case \*.zip
                 unzip $argv
-            case *.tar.bz2
+            case \*.tar.bz2
                 tar xvjf $argv
-            case *.tar.gz
+            case \*.tar.gz
                 tar xvzf $argv
-            case *.bz2
+            case \*.bz2
                 bunzip2 $argv
-            case *.gz
+            case \*.gz
                 gunzip $argv
-            case *.tar
+            case \*.tar
                 tar xvf $argv
-            case *.tbz2
+            case \*.tbz2
                 tar xvjf $argv
-            case *.tgz
+            case \*.tgz
                 tar xvzf $argv
-            case *.Z
+            case \*.Z
                 uncompress $argv
-            case *.7z
+            case \*.7z
                 7z x $argv
         end
     else
